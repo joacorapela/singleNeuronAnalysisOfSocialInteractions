c996d8c8e668f0024cf8f313aef721adf37ae353
Joaquin Rapela <joacorapela@gmail.com>
2021-04-08 13:49:45 +0100

    Changed train and classify, but I prefer the older implemetnation

--- code/src/classifiers.py
+++ code/src/classifiers.py
@@ -6,20 +6,26 @@ sys.path.append("../src")
 import probabilisticModels
 
 class NaiveBayes:
-    def train(self, x_by_class, model_class=probabilisticModels.Exponential):
+    def train(self, x, y, model_class=probabilisticModels.Exponential):
         self._models = {}
-        for class_label in x_by_class:
-            x = x_by_class[class_label]
+        unique_y = np.unique(y)
+        for class_label in unique_y:
+            # x_in_class_indices = np.where(y==class_label)[0]
+            x_in_class = np.array([x[i] for i, y_item in enumerate(y) if y_item==class_label])
             self._models[class_label] = model_class()
-            self._models[class_label].train(x=x)
+            self._models[class_label].train(x=x_in_class)
+        # pdb.set_trace()
 
-    def classify(self, x):
-        maxLogLikeValue = -np.Inf
-        maxLogLikeClass = None
-        for class_label, model in self._models.items():
-            logLike = model.logLikelihood(x=x)
-            # pdb.set_trace()
-            if logLike>maxLogLikeValue:
-                maxLogLikeValue = logLike
-                maxLogLikeClass = class_label
-        return maxLogLikeClass
+    def predict(self, x):
+        y = [None]*len(x)
+        for i, x_item in enumerate(x):
+            maxProbValue = -np.Inf
+            maxProbClass = None
+            for class_label, model in self._models.items():
+                prob = model.probability(x=x_item)
+                # pdb.set_trace()
+                if prob>maxProbValue:
+                    maxProbValue = prob
+                    maxProbClass = class_label
+            y[i] = maxProbClass
+        return y
